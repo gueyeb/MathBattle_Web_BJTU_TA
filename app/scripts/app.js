77951cfbfad5fd07f13ccd8611cb28644800b479
Babacar Gueye <babacar.gueye@epitech.eu>
2014-12-15 22:06:54 +0800

    basic architecture using yeoman angular

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mathBattleApp
+ * @description
+ * # mathBattleApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mathBattleApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
